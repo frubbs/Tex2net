13a23e35897c5f43326ccf7c90a5d8a8ebd80ad1
frubbs <rafaelhss@gmail.com>
2015-09-24 14:33:07 -0300

    Linebreak parametrizavel

--- text2net/src/main/java/com/text2net/Text2Net.java
+++ text2net/src/main/java/com/text2net/Text2Net.java
@@ -74,6 +74,9 @@ public class Text2Net {
 
 	private ConnectionQueryResult processRequest(String text, File gappFile) throws Exception {
 		ConnectionQueryResult result;
+		
+		System.out.println("Starting annotation with xapp: " + gappFile.getAbsolutePath());
+		
 		AnnotatedText annotatedText = new TextAnnotator().processString(gappFile, text);
 		
 		System.out.println("Finished Annotation. Starting connection detection");
